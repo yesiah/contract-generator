405e22455ac522cc24e655fe8ff34dd9da129cbd
yesiah <hyenhsiang@gmail.com>
2021-05-07 23:49:57 +0800

    Stop tracking test.py

--- test.py
+++
@@ -1,7 +0,0 @@
-from string import Formatter
-
-mystr = "how do {you} true this {on}"
-fieldnames = [fname for _, fname, _, _ in Formatter().parse(mystr) if fname]
-fieldvalues = ["replace_you", "replace_on"]
-d = dict(zip(fieldnames, fieldvalues))
-print(mystr.format(**d))
